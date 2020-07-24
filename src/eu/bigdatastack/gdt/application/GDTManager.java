4bb547b7be6055d95721aef6835582a0cca067cc
Richard McCreadie <richard.mccreadie@glasgow.ac.uk>
2020-07-24 13:34:33 +0100

    Reduced prometheus query scope to minimise return size

--- src/eu/bigdatastack/gdt/application/GDTManager.java
+++ src/eu/bigdatastack/gdt/application/GDTManager.java
@@ -1438,7 +1438,7 @@ public class GDTManager implements Manager {
 	 * @return
 	 */
 	public PerHourTimeSeries generatePerHourTimeSeries(String owner, String namespace, String metric) {
-		return prometheusDataClient.perHourAvg(owner, namespace, null, null, null, metric, "12h");
+		return prometheusDataClient.perHourAvg(owner, namespace, null, null, null, metric, "6h");
 	}
 	
 	/**
